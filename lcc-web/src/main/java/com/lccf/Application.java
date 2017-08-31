9888db119bf67e31c2418ea064a6c40f7cc97501
licc168 <liccwork@126.com>
2017-08-31 17:06:10 +0800

    优化springboot启动类

--- lcc-web/src/main/java/com/lccf/Application.java
+++ lcc-web/src/main/java/com/lccf/Application.java
@@ -1,19 +1,30 @@
 
 package com.lccf;
 
-import com.lccf.constants.LccfProperties;
-import com.lccf.util.SpringUtil;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+import com.lccf.config.LocaleConfiguration;
+import com.lccf.config.MappingConfiguration;
+import com.lccf.config.security.WebSecurityConfig;
+import com.lccf.constants.LccfProperties;
+import com.lccf.util.SpringUtil;
+
+@Configuration
+@EnableAutoConfiguration
 @EnableConfigurationProperties({ LccfProperties.class })
-@Import(SpringUtil.class)
-@ServletComponentScan
+@Import({
+    SpringUtil.class,
+    WebSecurityConfig.class,
+    LocaleConfiguration.class,
+    MappingConfiguration.class
+
+})
+@ComponentScan("com.lccf")
 public class Application {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
