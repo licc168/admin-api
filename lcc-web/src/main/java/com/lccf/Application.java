ff2d8f6f171f269e17a3471e5b994bf96c412b50
licc168 <liccwork@126.com>
2017-09-23 10:17:16 +0800

    换成tomcat部署war 并去掉默认的logback配置

--- lcc-web/src/main/java/com/lccf/Application.java
+++ lcc-web/src/main/java/com/lccf/Application.java
@@ -3,7 +3,9 @@ package com.lccf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -25,7 +27,13 @@ import com.lccf.util.SpringUtil;
 
 })
 @ComponentScan("com.lccf")
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
